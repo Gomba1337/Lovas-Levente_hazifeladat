10421d1c307a36b026bb8458a6bf780ff0198ceb
Gomba1337 <lvntlovas@gmail.com>
2025-11-10 18:29:05 +0100

    Update Eldöntés, kiválasztás, keresés 1.py

--- Eldöntés, kiválasztás, keresés 1.py
+++ Eldöntés, kiválasztás, keresés 1.py
@@ -1,7 +1,7 @@
 import random
 
 lista = []
-for _ in range(5):
+for x in range(5):
     szam = random.randint(1, 7)
     lista.append(szam)
 
@@ -13,3 +13,4 @@ if szam in lista:
         print(f"A {szam} szerepel a listában.")
 else:
         print(f"A {szam} nem szerepel a listában.")
+
